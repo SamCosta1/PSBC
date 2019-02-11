4d1644be9cb08d4f3952d713741dab2564b0e5c6
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-02-11 16:21:43 +0000

    first optimization attempt

---
+++ coursework/ex1/AppEm.m
@@ -0,0 +1,33 @@
+function [p, q] = AppEm (N)
+  emConstant = 0.577215664901533;
+  absDiff = @(value) abs(value - emConstant);
+
+
+  currentBestDiff = intmax;
+  currentBestPQ = [0, 0];
+  
+  for p = 1:N
+    lowerBound = ceil(5*p/3);
+    upperBound = min(2*p, N) - 1;
+    for q = lowerBound:upperBound
+      
+      if (p + q > N) 
+        continue;
+      endif
+      approximation = p / q;
+     
+      diffToEM = absDiff(approximation);
+
+      if (diffToEM < currentBestDiff)
+        currentBestDiff = diffToEM;
+        currentBestPQ = [p, q];
+      endif
+      
+    endfor
+  endfor
+  
+  p =  currentBestPQ(1);
+  q =  currentBestPQ(2);
+
+
+endfunction
---
+++ coursework/ex1/part1.m
@@ -0,0 +1,1 @@
+tic();[p, q] = AppEm(2019);elapsed = toc();elapsedpq
