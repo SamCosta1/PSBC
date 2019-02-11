d25acca2b43cd052befdb74555702297c4b89a59
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-02-11 17:28:47 +0000

    optimize

--- coursework/ex1/AppEm.m
+++ coursework/ex1/AppEm.m
@@ -1,26 +1,31 @@
 function [p, q] = AppEm (N)
   emConstant = 0.577215664901533;
-  absDiff = @(value) abs(value - emConstant);
 
+  if (N == 2)
+    p = 1;
+    q = 1;
+    return;
+  endif
+    absDiff = @(value) abs(value - emConstant);
 
   currentBestDiff = intmax;
   currentBestPQ = [0, 0];
   
   for p = 1:N
     lowerBound = ceil(5*p/3);
-    upperBound = min(2*p, N) - 1;
+    upperBound = min(2*p, N - p);
+    
     for q = lowerBound:upperBound
-      
-      if (p + q > N) 
-        continue;
-      endif
       approximation = p / q;
      
       diffToEM = absDiff(approximation);
 
-      if (diffToEM < currentBestDiff)
-        currentBestDiff = diffToEM;
-        currentBestPQ = [p, q];
+      if (diffToEM <= currentBestDiff)
+        
+        if (diffToEM < currentBestDiff || p + q < sum(currentBestPQ))
+          currentBestDiff = diffToEM;
+          currentBestPQ = [p, q];
+        endif
       endif
       
     endfor
