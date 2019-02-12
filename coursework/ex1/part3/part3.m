00af6138a366dd3b57a92e0efd8cf526fb08ab23
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-02-12 16:10:56 +0000

    slightly better

---
+++ coursework/ex1/part3/part3.m
@@ -0,0 +1,1 @@
+tic();perfectNumberNearest(360322019, 11826, 30384)elapsed = toc();elapsed
--- coursework/ex1/part3/perfectNumberNearest.m
+++ coursework/ex1/part3/perfectNumberNearest.m
@@ -1,3 +1,3 @@
-function result = perfectNumberNearest(N)    closest = 11826;  minDiff = N - 11826^2;  for i = 11826:30384    square = i^2;        if (isPerfect(square))       diff = abs(N - square);      if (diff < minDiff)        closest = i;        minDiff =  diff;       endif    endif    
+function result = perfectNumberNearest(N, lowerBound, upperBound)    closest = lowerBound;  minDiff = N - lowerBound^2;  for i = lowerBound:upperBound    square = i^2;        if (isPerfect(square))       diff = abs(N - square);      if (diff < minDiff)        closest = i;        minDiff =  diff;       endif    endif    
     endfor        result = closest;
 endfunction
