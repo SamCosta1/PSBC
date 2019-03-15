0546d4d9fa053c0a09de5a5abf0352c1dee410e9
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-15 20:37:56 +0000

    kinda works

--- coursework/ex2/main.m
+++ coursework/ex2/main.m
@@ -4,12 +4,24 @@ foxSpeed = 16;
 
 rabbitPos = [0, 0];
 foxPos = [250, -550];
-burrowPos = [600 600];
+burrowPos = [600, 600];
 
 warehouseNW = [200, 0];
 warehouseSW = [200 -400];
 
 [t, z] = ode45(@(t, z)modelODE(t, z, burrowPos, rabbitSpeed, foxSpeed, warehouseNW, warehouseSW), ...
-                [0 40], [rabbitPos foxPos]);
-                
-plot(z(:,1), z(:,2), z(:,3), z(:,4));
+                [0 400], [rabbitPos foxPos]);
+    
+##figure(1);    
+plot(z(:,1), z(:,2))
+hold on;
+plot(z(:,3), z(:,4));
+hold on;
+plot(burrowPos(1), burrowPos(2), 'k*');
+legend('Rabbit', 'Fox');
+hold off;
+
+##
+##figure(2);
+##
+##plot(t, z(:,1), t, z(:,3));
---
+++ coursework/ex2/stopEvent.m
@@ -0,0 +1,2 @@
+function [value,isterminal,direction] = stopEvent(t, z)    % z = [Rx, Ry, Fx, Fy]  value = z(2);  isterminal = 1;  direction = 1;
+endfunction
