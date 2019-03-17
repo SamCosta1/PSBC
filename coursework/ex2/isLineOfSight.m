68714c358b764a17b5b7b4c0e0be285ea4f08407
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-17 10:29:00 +0000

    simple version working well

--- coursework/ex2/isLineOfSight.m
+++ coursework/ex2/isLineOfSight.m
@@ -10,9 +10,9 @@ function lineOfSight = isLineOfSight (pos1, pos2, warehouseNW, warehouseSW)
   intersectionLeftY = warehouseNW(1) * diffPos(2) / diffPos(1);
 
   % The intersection between the fox's line of sight and the bottom wall's line
-  intersectionBottomY = warehouseSW(2) * diffPos(1) / diffPos(2)
+  intersectionBottomY = warehouseSW(2) * diffPos(1) / diffPos(2);
 
   lineOfSight = intersectionLeftY > warehouseNW(2) && intersectionLeftY < warehouseNW(2) ...
-    || warehouseSW(1) < intersectionBottomY;
+    || warehouseSW(1) < intersectionBottomY
 
 endfunction
--- coursework/ex2/main.m
+++ coursework/ex2/main.m
@@ -1,7 +1,9 @@
 
+# Speeds in meters per second
 rabbitSpeed = 13;
 foxSpeed = 16;
 
+# Coordinates in meters
 rabbitPos = [0, 0];
 foxPos = [250, -550];
 burrowPos = [600, 600];
@@ -9,17 +11,30 @@ burrowPos = [600, 600];
 warehouseNW = [200, 0];
 warehouseSW = [200 -400];
 
+options = odeset('Events',@(t,z)stopEvent(t,z, burrowPos), 'RelTol',1e-5);
 [t, z] = ode45(@(t, z)modelODE(t, z, burrowPos, rabbitSpeed, foxSpeed, warehouseNW, warehouseSW), ...
-                [0 400], [rabbitPos foxPos]);
+                [0 100], [rabbitPos foxPos], options);
     
-##figure(1);    
+figure(1);   
+patch([warehouseSW(1) 800 800 warehouseSW(1)], [warehouseSW(2) warehouseSW(2) warehouseNW(2) warehouseNW(2)], 'g');
+hold on; 
 plot(z(:,1), z(:,2))
 hold on;
 plot(z(:,3), z(:,4));
 hold on;
 plot(burrowPos(1), burrowPos(2), 'k*');
-legend('Rabbit', 'Fox');
+legend('Warehouse', 'Rabbit', 'Fox');
+
+
+d = 40
+##axis([600-d 600+d 600-d 600+d]);
 hold off;
+##
+##figure(2);
+dist = sqrt((z(:,4) - z(:,2)) .^ 2 + (z(:,1) - z(:,3)) .^ 2);
+
+##plot(t, dist);
+##hold off;
 
 ##
 ##figure(2);
