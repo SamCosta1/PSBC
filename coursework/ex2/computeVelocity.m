7a2fe52fd8c4294ae0efc021a266ebc453aa01c0
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-24 14:53:02 +0000

    Simple ode tidy now done

--- coursework/ex2/computeVelocity.m
+++ coursework/ex2/computeVelocity.m
@@ -2,15 +2,7 @@
 % return the x and y components of it's velocity
 function velocity = computeVelocity(speed, pos, target)
   Y = 2; # index of y in pos
-  X = 1; # index of x in pos  
-  velocity = zeros(sizeof(pos));
+  X = 1; # index of x in pos
   dist = distance(pos, target);
-  
-  yDistance = target(Y) - pos(Y);
-  xDistance = target(X) - pos(X);
-  
-  
-  velocity(X) = speed * (xDistance / dist);
-  velocity(Y) = speed * (yDistance / dist);
-
+  velocity = speed .* ((target - pos) ./ dist);
 end
