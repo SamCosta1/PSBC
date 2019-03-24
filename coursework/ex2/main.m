e05c03edf8eede892c77ea72302114a627eec401
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-24 15:25:54 +0000

    Pretty output messages

--- coursework/ex2/main.m
+++ coursework/ex2/main.m
@@ -4,9 +4,9 @@ rabbitSpeed = 13;
 foxSpeed = 16;
 
 # Coordinates in meters
-rabbitPos = [0, 0];
-foxPos = [250, -550];
-burrowPos = [600, 600];
+rabbitPos = [0, -400];
+foxPos = [250, 150];
+burrowPos = [600, -1e3];
 
 warehouseNW = [200, 0];
 warehouseSW = [200 -400];
@@ -17,15 +17,23 @@ rabbitSpeedRate = 7e-4;
 
 options = odeset('Events', @(t,z)stopEvent(t,z, burrowPos), 'MaxStep', 0.1);
 
-[tsimple, zsimple] = ode45(@(t, z)modelODE(t, z, burrowPos, rabbitSpeed, foxSpeed, warehouseNW, warehouseSW), ...
-                [0 300], [rabbitPos foxPos], options);
+messages = {'The fox caught the rabbit', 'The rabbit reached the burrow'};
+printSol = @(time, index) printf("\nAt %f, %s\n\n", time, messages{index});
 
-[tcomplex, zcomplex] = ode45(@(t, z) complexModelODE(t, z, burrowPos, ...
+warning('off', 'integrate_adaptive:unexpected_termination');
+[~, zsimple, t1Event, ~, i1Event] = ode45(@(t, z) modelODE(t, z, burrowPos, ...
+                                            rabbitSpeed, foxSpeed,...
+                                            warehouseNW, warehouseSW), ...
+                [0 300], [rabbitPos foxPos], options);
+                
+
+printSol(t1Event, i1Event);
+[~, zcomplex, t2Event, ~, i2Event] = ode45(@(t, z) complexModelODE(t, z, burrowPos, ...
                                        rabbitSpeed, foxSpeed, ...
                                        warehouseNW, warehouseSW, ...
                                        rabbitSpeedRate, foxSpeedRate), ...
                 [0 300], [rabbitPos foxPos 0 0], options);
     
-
-plotScene(burrowPos, warehouseNW, warehouseSW, zcomplex, 1);
-plotScene(burrowPos, warehouseNW, warehouseSW, zsimple, 2);
+printSol(t2Event, i2Event);
+##plotScene(burrowPos, warehouseNW, warehouseSW, zcomplex, 1);
+##plotScene(burrowPos, warehouseNW, warehouseSW, zsimple, 2);
