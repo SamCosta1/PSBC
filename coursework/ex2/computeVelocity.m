52f060c256b15496c7f19489bef965971f846df5
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-24 14:49:22 +0000

    Velocities tweaked still works

---
+++ coursework/ex2/computeVelocity.m
@@ -0,0 +1,16 @@
+% For an entity directed at a target with a given speed
+% return the x and y components of it's velocity
+function velocity = computeVelocity(speed, pos, target)
+  Y = 2; # index of y in pos
+  X = 1; # index of x in pos  
+  velocity = zeros(sizeof(pos));
+  dist = distance(pos, target);
+  
+  yDistance = target(Y) - pos(Y);
+  xDistance = target(X) - pos(X);
+  
+  
+  velocity(X) = speed * (xDistance / dist);
+  velocity(Y) = speed * (yDistance / dist);
+
+end
--- coursework/ex2/distance.m
+++ coursework/ex2/distance.m
@@ -1,2 +1,7 @@
-function dist = distance(p1, p2)   dist = (p1(2) - p2(2)) ^ 2 + (p1(1) - p2(1))^2;
+% Distance between two points P1 and P2
+function dist = distance(p1, p2) 
+  Y = 2; # index of y in pos
+  X = 1; # index of x in pos
+  
+  dist = sqrt((p1(Y) - p2(Y)) ^ 2 + (p1(X) - p2(X))^2);
 endfunction
