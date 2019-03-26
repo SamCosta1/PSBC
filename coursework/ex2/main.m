a1c7e65b993289872881253b7053e587b978e8ea
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-26 18:53:26 +0000

    before experiment

--- coursework/ex2/main.m
+++ coursework/ex2/main.m
@@ -1,3 +1,4 @@
+warning('off', 'integrate_adaptive:unexpected_termination');
 
 # Speeds in meters per second
 rabbitSpeed = 13;
@@ -20,20 +21,21 @@ options = odeset('Events', @(t,z)stopEvent(t,z, burrowPos), 'MaxStep', 0.1);
 messages = {'The fox caught the rabbit', 'The rabbit reached the burrow'};
 printSol = @(time, index) printf("\nAt %f, %s\n\n", time, messages{index});
 
-warning('off', 'integrate_adaptive:unexpected_termination');
-[~, zsimple, t1Event, ~, i1Event] = ode45(@(t, z) modelODE(t, z, burrowPos, ...
-                                            rabbitSpeed, foxSpeed,...
-                                            warehouseNW, warehouseSW), ...
-                [0 300], [rabbitPos foxPos], options);
-                
+##[~, zsimple, t1Event, ~, i1Event] = ode45(@(t, z) modelODE(t, z, burrowPos, ...
+##                                            rabbitSpeed, foxSpeed,...
+##                                            warehouseNW, warehouseSW), ...
+##                [0 300], [rabbitPos foxPos], options);
+##                
+##
+##printSol(t1Event, i1Event);
 
-printSol(t1Event, i1Event);
 [~, zcomplex, t2Event, ~, i2Event] = ode45(@(t, z) complexModelODE(t, z, burrowPos, ...
                                        rabbitSpeed, foxSpeed, ...
                                        warehouseNW, warehouseSW, ...
                                        rabbitSpeedRate, foxSpeedRate), ...
-                [0 300], [rabbitPos foxPos 0 0], options);
+                [0 100], [rabbitPos foxPos 0 0], options);
     
 printSol(t2Event, i2Event);
+
 plotScene(burrowPos, warehouseNW, warehouseSW, zcomplex, 1);
-plotScene(burrowPos, warehouseNW, warehouseSW, zsimple, 2);
+##plotScene(burrowPos, warehouseNW, warehouseSW, zsimple, 2);
