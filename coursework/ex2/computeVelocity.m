ec6a6dfd2d57f48254ff79a8e6905dbcea61a6fb
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-27 12:22:48 +0000

    velocities done

--- coursework/ex2/computeVelocity.m
+++ coursework/ex2/computeVelocity.m
@@ -1,8 +1,6 @@
 % For an entity directed at a target with a given speed
 % return the x and y components of it's velocity
 function velocity = computeVelocity(speed, pos, target)
-  Y = 2; # index of y in pos
-  X = 1; # index of x in pos
   dist = distance(pos, target);
   velocity = speed .* ((target - pos) ./ dist);
 end
--- coursework/ex2/distance.m
+++ coursework/ex2/distance.m
@@ -1,7 +1,4 @@
 % Distance between two points P1 and P2
 function dist = distance(p1, p2) 
-  Y = 2; # index of y in pos
-  X = 1; # index of x in pos
-  
-  dist = sqrt((p1(Y) - p2(Y)) ^ 2 + (p1(X) - p2(X))^2);
+  dist = sqrt(sum((p2 - p1) .^ 2));
 endfunction
