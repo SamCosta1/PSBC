eb78a4b4320c56230af1508a91992ab74eaaf58d
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-27 15:31:22 +0000

    target done

--- coursework/ex2/main.m
+++ coursework/ex2/main.m
@@ -1,10 +1,10 @@
 warning('off', 'integrate_adaptive:unexpected_termination');
 
-# Speeds in meters per second
+% Speeds in meters per second
 rabbitSpeed = 13;
 foxSpeed = 16;
 
-# Coordinates in meters
+% Coordinates in meters
 rabbitPos = [0, 0];
 foxPos = [250, -550];
 burrowPos = [600, 600];
@@ -12,7 +12,7 @@ burrowPos = [600, 600];
 warehouseNW = [200, 0];
 warehouseSW = [200 -400];
 
-# The rates of diminishing speeds
+% The rates of diminishing speeds
 foxSpeedRate = 2e-4;
 rabbitSpeedRate = 7e-4;
 
@@ -21,13 +21,13 @@ options = odeset('Events', @(t,z)stopEvent(t,z, burrowPos), 'MaxStep', 0.1);
 messages = {'The fox caught the rabbit', 'The rabbit reached the burrow'};
 printSol = @(time, index) printf("\nAt %f, %s\n\n", time, messages{index});
 
-##[~, zsimple, t1Event, ~, i1Event] = ode45(@(t, z) modelODE(t, z, burrowPos, ...
-##                                            rabbitSpeed, foxSpeed,...
-##                                            warehouseNW, warehouseSW), ...
-##                [0 300], [rabbitPos foxPos], options);
-##                
-##
-##printSol(t1Event, i1Event);
+%%[~, zsimple, t1Event, ~, i1Event] = ode45(@(t, z) modelODE(t, z, burrowPos, ...
+%%                                            rabbitSpeed, foxSpeed,...
+%%                                            warehouseNW, warehouseSW), ...
+%%                [0 300], [rabbitPos foxPos], options);
+%%                
+%%
+%%printSol(t1Event, i1Event);
 
 [~, zcomplex, t2Event, ~, i2Event] = ode45(@(t, z) complexModelODE(t, z, burrowPos, ...
                                        rabbitSpeed, foxSpeed, ...
@@ -38,4 +38,4 @@ printSol = @(time, index) printf("\nAt %f, %s\n\n", time, messages{index});
 printSol(t2Event, i2Event);
 
 plotScene(burrowPos, warehouseNW, warehouseSW, zcomplex, 1);
-##plotScene(burrowPos, warehouseNW, warehouseSW, zsimple, 2);
+%%plotScene(burrowPos, warehouseNW, warehouseSW, zsimple, 2);
