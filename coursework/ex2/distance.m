48c793da00b77f6496be10f5a5f96d358d952883
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-03-28 13:00:19 +0000

    part1 done

--- coursework/ex2/distance.m
+++ coursework/ex2/distance.m
@@ -1,4 +1,4 @@
 % Distance between two points P1 and P2
 function dist = distance(p1, p2) 
   dist = sqrt(sum((p2 - p1) .^ 2));
-endfunction
+end
---
+++ coursework/ex2/part1.m
@@ -0,0 +1,27 @@
+warning('off', 'integrate_adaptive:unexpected_termination');
+
+% Speeds in meters per second
+rabbitSpeed = 13;
+foxSpeed = 16;
+
+% Coordinates in meters
+rabbitPos = [0, 0];
+foxPos = [250, -550];
+burrowPos = [600, 600];
+
+warehouseNW = [200, 0];
+warehouseSW = [200 -400];
+
+messages = {'the fox caught the rabbit', 'the rabbit reached the burrow'};
+printSol = @(time, index) printf("\nAt time %f, %s\n\n", time, messages{index});
+
+options = odeset('Events', @(t,z)stopEvent(t,z, burrowPos), 'MaxStep', 0.1);
+
+[~, z, tEvent, ~, i1vent] = ode45(@(t, z) modelODE(t, z, burrowPos, ...
+                                            rabbitSpeed, foxSpeed,...
+                                            warehouseNW, warehouseSW), ...
+                [0 300], [rabbitPos foxPos], options);
+                
+
+printSol(tEvent, iEvent);
+plotScene(burrowPos, warehouseNW, warehouseSW, z, 1, "./report/simpleModel.eps");
