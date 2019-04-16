0c74233e72f0b8720bcc5c900f64ccc4a4a13586
ethe3765 <dthetet@thet.com>
2019-04-16 17:45:32 +0100

    kinda working out gradients

---
+++ coursework/ex3/slopeAndIntercept.m
@@ -0,0 +1,2 @@
+function [slope, intercept] = slopeAndIntercept(x, y)    A = [sum(x.^2) sum(x);...       sum(x)    numel(x)];  lambda = [sum(x .* y); sum(y)];    solved = A \ lambda;  slope = solved(1);  intercept = solved(2);
+end
